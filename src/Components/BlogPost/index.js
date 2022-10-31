65bad18b0c8d512afb87aaecd29ad77801d2d6ca
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-31 18:07:15 -0500

    useNavigate &  onBack function to return to /blog

--- src/Components/BlogPost/index.js
+++ src/Components/BlogPost/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { useBlogData } from '../Hooks/useBlogData'
 
 function BlogPost() {
@@ -7,9 +7,17 @@ function BlogPost() {
   const blogData=useBlogData()
   const blogPost=blogData.find(post=>post.slug===slug)
   const {title,author,content}=blogPost
+  // React Hook for Navigation & function to return to /blog
+  const navigate=useNavigate()
+  const onBack=(slug)=>{
+    navigate(slug)
+  }
   return (
     <>
     <h2>{title}</h2>
+    <button
+      onClick={()=>onBack('/blog')}
+    >&#8617;</button>
     <p>{author}</p>
     <p>{content}</p>
     </>
